dc82d144e479f19a60afd02d9c4d56b1593324c7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-11-30 16:47:48 -0500

    Fixeed semantic structure of the webpage.

--- app/code-of-conduct/page.tsx
+++ app/code-of-conduct/page.tsx
@@ -4,20 +4,20 @@ import Link from "next/link";
 export default function CoC() {
   return (
     <main className="wrapper pb-6 [&_p]:text-sm [&_li]:text-sm [&_h2]:text-4xl [&_h4]:text-2xl">
-      <h2 className="text-center text-5xl p-3 m-3 uppercase font-light">
+      <h1 className="text-center text-5xl p-3 m-3 uppercase font-light">
         Code of Conduct
-      </h2>
+      </h1>
 
       <article className="">
         {/* SCOPE */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">Scope</h4>
+          <h2 className="text-3xl uppercase font-light pb-2">Scope</h2>
           <p>{CoC_Contents.SCOPE_TEXT}</p>
         </div>
 
         {/* ENFORCEMENT */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">Enforcement</h4>
+          <h2 className="text-3xl uppercase font-light pb-2">Enforcement</h2>
           <p className="pb-2">{CoC_Contents.ENFORCEMENT_TEXT}</p>
           <ul>
             {CoC_Contents.ENFORCEMENT_LIST.map((list) => (
@@ -33,9 +33,9 @@ export default function CoC() {
 
         {/* UNACCEPTABLE BEHAVIOR */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">
+          <h2 className="text-3xl uppercase font-light pb-2">
             Unacceptable Behavior
-          </h4>
+          </h2>
           <ul>
             {CoC_Contents.UNACCEPTABLE_BEHAVIOR.map((list) => (
               <li key={list.id} className="my-2 ml-10">
@@ -59,9 +59,9 @@ export default function CoC() {
 
         {/* EXAMPLES OF UNACCEPTABLES */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">
+          <h2 className="text-3xl uppercase font-light pb-2">
             Examples of Unaccepted Behaviors:
-          </h4>
+          </h2>
           <ul>
             {CoC_Contents.EXAMPLES_UNACCEPTABLES.map((item) => (
               <li key={item} className="list-disc ml-10 my-1.5">
@@ -73,9 +73,9 @@ export default function CoC() {
 
         {/* ACCEPTABLE BEHAVIOR */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">
+          <h2 className="text-3xl uppercase font-light pb-2">
             Examples of Acceptable Behaviors
-          </h4>
+          </h2>
           <ul>
             {CoC_Contents.EXAMPLES_ACCEPTABLES.map((item) => (
               <li key={item} className="list-disc ml-10 my-1.5">
@@ -87,9 +87,9 @@ export default function CoC() {
 
         {/* REFERRAL POLICY */}
         <div className="my-6">
-          <h4 className="text-3xl uppercase font-light pb-2">
+          <h2 className="text-3xl uppercase font-light pb-2">
             Referral Policy
-          </h4>
+          </h2>
           <p>{CoC_Contents.REFERRAL_POLICY_TEXT}</p>
           <ul>
             {CoC_Contents.REFERRAL_POLICIES.map((list) => (
